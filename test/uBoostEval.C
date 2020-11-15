42ece7554da51a8fb49e8ac70f68b76bfb9a0b2f
Yipeng Sun <syp@umd.edu>
2020-11-16 00:50:06 +0100

    Reformat code.

--- test/uBoostEval.C
+++ test/uBoostEval.C
@@ -4,102 +4,105 @@
 #include <string>
 using namespace std;
 
-#include "TChain.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TString.h"
-#include "TObjString.h"
-#include "TSystem.h"
-#include "TROOT.h"
-#include "TPluginManager.h"
-#include "TLatex.h"
-#include "TH2F.h"
 #include "TCanvas.h"
-#include "TMath.h"
-#include "TGraph.h"
+#include "TChain.h"
 #include "TColor.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TH2F.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "TObjString.h"
+#include "TPluginManager.h"
+#include "TROOT.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TTree.h"
 
 #if not defined(__CINT__) || defined(__MAKECINT__)
 // needs to be included when makecint runs (ACLIC)
 #include "TMVA/Factory.h"
-#include "TMVA/Tools.h"
-#include "TMVA/Reader.h"
 #include "TMVA/MethodCuts.h"
+#include "TMVA/Reader.h"
+#include "TMVA/Tools.h"
 #endif
 
 using namespace TMVA;
 
-void uBoostEval(){
+void uBoostEval() {
 
   // gSystem->Load("$TMVALOC/lib/libTMVA.1.so");
 
   TMVA::Reader *reader;
-  float m2ab,m2ac,y1,y2,y3,pmin,BDT;
+  float m2ab, m2ac, y1, y2, y3, pmin, BDT;
   TString weightName = "TMVA_UBDT.weights.xml";
   TMVA::Tools::Instance();
   reader = new TMVA::Reader("!Color:Silent");
-  reader->AddVariable("Y1",&y1);
-  reader->AddVariable("Y2",&y2);
-  reader->AddVariable("Y3",&y3);
-  reader->AddSpectator("M2AB",&m2ab);
-  reader->AddSpectator("M2AC",&m2ac);
-  reader->BookMVA("UBDT method",weightName);
+  reader->AddVariable("Y1", &y1);
+  reader->AddVariable("Y2", &y2);
+  reader->AddVariable("Y3", &y3);
+  reader->AddSpectator("M2AB", &m2ab);
+  reader->AddSpectator("M2AC", &m2ac);
+  reader->BookMVA("UBDT method", weightName);
 
   // input signal training sample
   TFile *inputS = new TFile("signal.root");
-  TTree* ts = (TTree*)inputS->Get("T");
-  ts->SetBranchAddress("M2AB",&m2ab);
-  ts->SetBranchAddress("M2AC",&m2ac);
-  ts->SetBranchAddress("Y1",&y1);
-  ts->SetBranchAddress("Y2",&y2);
-  ts->SetBranchAddress("Y3",&y3);
-  ts->SetBranchAddress("PMIN",&pmin);
+  TTree *ts = (TTree *)inputS->Get("T");
+  ts->SetBranchAddress("M2AB", &m2ab);
+  ts->SetBranchAddress("M2AC", &m2ac);
+  ts->SetBranchAddress("Y1", &y1);
+  ts->SetBranchAddress("Y2", &y2);
+  ts->SetBranchAddress("Y3", &y3);
+  ts->SetBranchAddress("PMIN", &pmin);
   int ns = (int)ts->GetEntries();
 
   // input background training sample
   TFile *inputB = new TFile("bkgd.root");
-  TTree* tb = (TTree*)inputB->Get("T");
-  tb->SetBranchAddress("M2AB",&m2ab);
-  tb->SetBranchAddress("M2AC",&m2ac);
-  tb->SetBranchAddress("Y1",&y1);
-  tb->SetBranchAddress("Y2",&y2);
-  tb->SetBranchAddress("Y3",&y3);
-  tb->SetBranchAddress("PMIN",&pmin);
+  TTree *tb = (TTree *)inputB->Get("T");
+  tb->SetBranchAddress("M2AB", &m2ab);
+  tb->SetBranchAddress("M2AC", &m2ac);
+  tb->SetBranchAddress("Y1", &y1);
+  tb->SetBranchAddress("Y2", &y2);
+  tb->SetBranchAddress("Y3", &y3);
+  tb->SetBranchAddress("PMIN", &pmin);
   int nb = (int)tb->GetEntries();
 
   // input high statistics signal evaluation sample
   TFile *input = new TFile("signal5e5.root");
-  TTree* t5e5 = (TTree*)input->Get("T");
-  t5e5->SetBranchAddress("M2AB",&m2ab);
-  t5e5->SetBranchAddress("M2AC",&m2ac);
-  t5e5->SetBranchAddress("Y1",&y1);
-  t5e5->SetBranchAddress("Y2",&y2);
-  t5e5->SetBranchAddress("Y3",&y3);
-  t5e5->SetBranchAddress("PMIN",&pmin);
+  TTree *t5e5 = (TTree *)input->Get("T");
+  t5e5->SetBranchAddress("M2AB", &m2ab);
+  t5e5->SetBranchAddress("M2AC", &m2ac);
+  t5e5->SetBranchAddress("Y1", &y1);
+  t5e5->SetBranchAddress("Y2", &y2);
+  t5e5->SetBranchAddress("Y3", &y3);
+  t5e5->SetBranchAddress("PMIN", &pmin);
   int nHighStat = (int)t5e5->GetEntries();
 
-  cout<<"Done loading input data"<<endl;
+  cout << "Done loading input data" << endl;
 
   // write uBDT response values to trees
-  TFile *outputSignalResponse = new TFile("signalResp.root","recreate");
-  TTree *tsResponse = new TTree("TResponse","");
+  TFile *outputSignalResponse = new TFile("signalResp.root", "recreate");
+  TTree *tsResponse = new TTree("TResponse", "");
   float signalResponse;
-  tsResponse->Branch("RESPONSE",&signalResponse);
+  tsResponse->Branch("RESPONSE", &signalResponse);
 
-  TFile *outputBkgdResponse = new TFile("bkgdResp.root","recreate");
-  TTree *tbResponse = new TTree("TResponse","");
+  TFile *outputBkgdResponse = new TFile("bkgdResp.root", "recreate");
+  TTree *tbResponse = new TTree("TResponse", "");
   float bkgdResponse;
-  tbResponse->Branch("RESPONSE",&bkgdResponse);
+  tbResponse->Branch("RESPONSE", &bkgdResponse);
 
-  TFile *outputSignal5e5Response = new TFile("signal5e5Resp.root","recreate");
-  TTree *ts5e5Response = new TTree("TResponse","");
+  TFile *outputSignal5e5Response = new TFile("signal5e5Resp.root", "recreate");
+  TTree *ts5e5Response = new TTree("TResponse", "");
   float signal5e5Response;
-  ts5e5Response->Branch("RESPONSE",&signal5e5Response);
+  ts5e5Response->Branch("RESPONSE", &signal5e5Response);
 
   // signal response distribution
-  cout<<endl<<"Processing "<<ns<<" signal events from train/test sample:"<<endl;
-  for(int e = 0; e < ns; e++){
-    if(e % 10000 == 0) cout<<"...... "<<e<<" events complete"<<endl;
+  cout << endl
+       << "Processing " << ns
+       << " signal events from train/test sample:" << endl;
+  for (int e = 0; e < ns; e++) {
+    if (e % 10000 == 0)
+      cout << "...... " << e << " events complete" << endl;
     ts->GetEntry(e);
     BDT = reader->EvaluateMVA("UBDT method");
     signalResponse = BDT;
@@ -107,9 +110,12 @@ void uBoostEval(){
   }
 
   // background response distribution
-  cout<<endl<<"Processing "<<ns<<" background events from train/test sample:"<<endl;
-  for(int e3 = 0; e3 < nb; e3++){
-    if(e3 % 10000 == 0) cout<<"...... "<<e3<<" events complete"<<endl;
+  cout << endl
+       << "Processing " << ns
+       << " background events from train/test sample:" << endl;
+  for (int e3 = 0; e3 < nb; e3++) {
+    if (e3 % 10000 == 0)
+      cout << "...... " << e3 << " events complete" << endl;
     tb->GetEntry(e3);
     BDT = reader->EvaluateMVA("UBDT method");
     bkgdResponse = BDT;
@@ -117,9 +123,12 @@ void uBoostEval(){
   }
 
   // high stats signal response distribution
-  cout<<endl<<"Processing "<<nHighStat<<" signal events from independent high statisitics sample:"<<endl;
-  for(int e4 = 0; e4 < nHighStat; e4++){
-    if(e4 % 100000 == 0) cout<<"...... "<<e4<<" events complete"<<endl;
+  cout << endl
+       << "Processing " << nHighStat
+       << " signal events from independent high statisitics sample:" << endl;
+  for (int e4 = 0; e4 < nHighStat; e4++) {
+    if (e4 % 100000 == 0)
+      cout << "...... " << e4 << " events complete" << endl;
     t5e5->GetEntry(e4);
     BDT = reader->EvaluateMVA("UBDT method");
     signal5e5Response = BDT;
--- test/uBoostTrain.C
+++ test/uBoostTrain.C
@@ -6,60 +6,69 @@ using namespace std;
 
 #include "TChain.h"
 #include "TFile.h"
-#include "TTree.h"
-#include "TString.h"
 #include "TObjString.h"
-#include "TSystem.h"
-#include "TROOT.h"
 #include "TPluginManager.h"
+#include "TROOT.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TTree.h"
 
 #if not defined(__CINT__) || defined(__MAKECINT__)
 // needs to be included when makecint runs (ACLIC)
 #include "TMVA/Factory.h"
-#include "TMVA/Tools.h"
-#include "TMVA/Reader.h"
 #include "TMVA/MethodCuts.h"
+#include "TMVA/Reader.h"
+#include "TMVA/Tools.h"
 #endif
 
 using namespace TMVA;
 
-void uBoostTrain(){
+void uBoostTrain() {
 
   gSystem->Load("$TMVALOC/lib/libTMVA.1.so");
 
   TMVA::Tools::Instance();
   TString weightName = "TMVA";
 
-  TFile *outFile = new TFile(weightName+"UBDT.root","recreate");
-  TMVA::Factory *factory = new TMVA::Factory(weightName, outFile,"!V:!Silent:Color:DrawProgressBar:Transformations=I" );
+  TFile *outFile = new TFile(weightName + "UBDT.root", "recreate");
+  TMVA::Factory *factory =
+      new TMVA::Factory(weightName, outFile,
+                        "!V:!Silent:Color:DrawProgressBar:Transformations=I");
 
-  //variables used in BDT training
-  factory->AddVariable("Y1","y_{1}","",'F');
-  factory->AddVariable("Y2","y_{2}","",'F');
-  factory->AddVariable("Y3","y_{3}","",'F');
+  // variables used in BDT training
+  factory->AddVariable("Y1", "y_{1}", "", 'F');
+  factory->AddVariable("Y2", "y_{2}", "", 'F');
+  factory->AddVariable("Y3", "y_{3}", "", 'F');
 
   // variables which uniformity is desired (used to define kNN)
-  factory->AddSpectator("M2AB","M_{AB}^{2}","GeV/c^{2}",'F');
-  factory->AddSpectator("M2AC","M_{AC}^{2}","GeV/c^{2}",'F');
+  factory->AddSpectator("M2AB", "M_{AB}^{2}", "GeV/c^{2}", 'F');
+  factory->AddSpectator("M2AC", "M_{AC}^{2}", "GeV/c^{2}", 'F');
 
   // get data
   TFile *fs = new TFile("signal.root");
   TFile *fb = new TFile("bkgd.root");
-  TTree *ts = (TTree*)fs->Get("T");
-  TTree *tb = (TTree*)fb->Get("T");
+  TTree *ts = (TTree *)fs->Get("T");
+  TTree *tb = (TTree *)fb->Get("T");
 
-  factory->AddSignalTree(ts,1.);
-  factory->AddBackgroundTree(tb,1.);
+  factory->AddSignalTree(ts, 1.);
+  factory->AddBackgroundTree(tb, 1.);
 
-  factory->SetSignalWeightExpression("W");      //eventwise weights
-  factory->SetBackgroundWeightExpression("W");  //eventwise weights
+  factory->SetSignalWeightExpression("W");     // eventwise weights
+  factory->SetBackgroundWeightExpression("W"); // eventwise weights
 
-  factory->PrepareTrainingAndTestTree("","","nTrain_Signal=0:nTrain_Background=0:SplitMode=Block:NormMode=NumEvents:!V" );
+  factory->PrepareTrainingAndTestTree("", "",
+                                      "nTrain_Signal=0:nTrain_Background=0:"
+                                      "SplitMode=Block:NormMode=NumEvents:!V");
 
-  TString bdt = "!H:!V:NTrees=100:nEventsMin=100:MaxDepth=4:BoostType=AdaBoost:AdaBoostBeta=1.0:PruneMethod=NoPruning:SeparationType=GiniIndex:nCuts=200";
-  factory->BookMethod(TMVA::Types::kBDT,"BDT",bdt);
-  bdt+=":uBoostFlag=1"; // add flag to use uBoost
-  factory->BookMethod(TMVA::Types::kUBDT,"UBDT","nKNN=100:UBDT_Num=10:UBDT_MinEffic=0.0:UBDT_MaxEffic=1.0:"+bdt); // 100 efficiency steps between 0 and 100%
+  TString bdt = "!H:!V:NTrees=100:nEventsMin=100:MaxDepth=4:BoostType=AdaBoost:"
+                "AdaBoostBeta=1.0:PruneMethod=NoPruning:SeparationType="
+                "GiniIndex:nCuts=200";
+  factory->BookMethod(TMVA::Types::kBDT, "BDT", bdt);
+  bdt += ":uBoostFlag=1"; // add flag to use uBoost
+  factory->BookMethod(
+      TMVA::Types::kUBDT, "UBDT",
+      "nKNN=100:UBDT_Num=10:UBDT_MinEffic=0.0:UBDT_MaxEffic=1.0:" +
+          bdt); // 100 efficiency steps between 0 and 100%
 
   factory->TrainAllMethods();
   factory->TestAllMethods();
