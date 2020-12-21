b3af45340e710871b703b47b039bde75b200a66f
Yipeng Sun <syp@umd.edu>
2020-12-21 22:59:01 +0100

    Adapt the helper script.

--- run2-pid/run.sh
+++ run2-pid/run.sh
@@ -2,11 +2,11 @@
 
 case $3 in
     debug)
-        run gaudirun.py $2 $1 \
+        lb-run Castelao/latest gaudirun.py ./reco_pp.py $1 \
             --option="from Configurables import DaVinci; DaVinci().EvtMax = 5000"
         ;;
 
     *)
-        run gaudirun.py $2 $1
+        lb-run Castelao/latest gaudirun.py ./reco_pp.py $1
         ;;
 esac
