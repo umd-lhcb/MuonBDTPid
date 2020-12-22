32dc87c87a54fc4a58c2eeef06d8c0c0f53496a9
Yipeng Sun <syp@umd.edu>
2020-12-23 00:25:09 +0100

    WIP #4: Use correct path to sPlot ntuples.

--- run2-pid/conds/cond-pid-2015-lxplus.py
+++ run2-pid/conds/cond-pid-2015-lxplus.py
@@ -8,14 +8,17 @@ DaVinci().TupleFile = "pid.root"
 from Configurables import SWeightsTableFiles
 
 SWeightsTableFiles(
-    sTableMagUpFile="../splots/splot_tables-2015-mu.root",
-    sTableMagDownFile="../splots/splot_tables-2015-md.root"
+    sTableMagUpFile="./splots/splot_tables-2015-mu.root",
+    sTableMagDownFile="./splots/splot_tables-2015-md.root"
 )
 
 
 from GaudiConf import IOHelper
+from os import environ
+
+HOME = environ['HOME']
 
 IOHelper().inputFiles([
-    '/afs/cern.ch/user/s/suny/eos/run2-pid/data-2015-mu/00050412_00000001_4.fullturbo.dst',
-    '/afs/cern.ch/user/s/suny/eos/run2-pid/data-2015-mu/00050412_00000002_4.fullturbo.dst',
+    HOME+'/eos/run2-pid/data-2015-mu/00050412_00000001_4.fullturbo.dst',
+    HOME+'/eos/run2-pid/data-2015-mu/00050412_00000002_4.fullturbo.dst',
 ], clear=True)
