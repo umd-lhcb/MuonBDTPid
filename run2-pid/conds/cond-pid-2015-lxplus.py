b87c9f05e70089c107dfaf2b575abd6218bde723
Yipeng Sun <syp@umd.edu>
2020-12-22 16:31:30 +0100

    WIP #4: Add splot ntuples as inputs in lxplus condition file.

--- run2-pid/conds/cond-pid-2015-lxplus.py
+++ run2-pid/conds/cond-pid-2015-lxplus.py
@@ -5,17 +5,15 @@ DaVinci().EvtMax = -1
 DaVinci().TupleFile = "pid.root"
 
 
-# from Configurables import SWeightsTableFiles
-# import os
-#
-# SWeightsTableFiles(
-#     sTableMagUpFile=os.environ["PIDCALIBROOT"] + "/sTables/sPlotTables-2015MagUp.root",
-#     sTableMagDownFile=os.environ["PIDCALIBROOT"]
-#     + "/sTables/sPlotTables-2015MagDown.root",
-# )
+from Configurables import SWeightsTableFiles
+
+SWeightsTableFiles(
+    sTableMagUpFile="../splots/splot_tables-2015-mu.root",
+    sTableMagDownFile="../splots/splot_tables-2015-md.root"
+)
+
 
 from GaudiConf import IOHelper
-
 
 IOHelper().inputFiles([
     '/afs/cern.ch/user/s/suny/eos/run2-pid/data-2015-mu/00050412_00000001_4.fullturbo.dst',
