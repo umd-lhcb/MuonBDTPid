423fe912ae9a982c023359648888c895b53c0466
Yipeng Sun <syp@umd.edu>
2021-01-21 23:31:56 +0100

    Only include the bare bone IO helper for lxplus.

--- run2-pid/conds/cond-pid-2015-lxplus.py
+++ run2-pid/conds/cond-pid-2015-lxplus.py
@@ -1,21 +1,7 @@
-from Configurables import DaVinci
-
-DaVinci().DataType = "2015"
-
-
-from Configurables import SWeightsTableFiles
-
-SWeightsTableFiles(
-    sTableMagUpFile="./splots/splot_tables-2015-mu.root",
-    sTableMagDownFile="./splots/splot_tables-2015-md.root"
-)
-
-
 from GaudiConf import IOHelper
 from os import environ
 
 HOME = environ['HOME']
-
 IOHelper().inputFiles([
     HOME+'/eos/run2-pid/data-2015-mu/00050412_00000001_4.fullturbo.dst',
     HOME+'/eos/run2-pid/data-2015-mu/00050412_00000002_4.fullturbo.dst',
