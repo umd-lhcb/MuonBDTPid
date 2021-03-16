75f572d00ed98d90e4eec52f1a8044b2128a19d9
Yipeng Sun <syp@umd.edu>
2021-03-17 00:30:58 +0100

    Remove local Castelao scripts since they don't work and no future work planned for them.

--- run2-pid/conds/cond-pid-2015-lxplus.py
+++
@@ -1,8 +0,0 @@
-from GaudiConf import IOHelper
-from os import environ
-
-HOME = environ['HOME']
-IOHelper().inputFiles([
-    HOME+'/eos/run2-pid/data-2015-mu/00050412_00000001_4.fullturbo.dst',
-    HOME+'/eos/run2-pid/data-2015-mu/00050412_00000002_4.fullturbo.dst',
-], clear=True)
