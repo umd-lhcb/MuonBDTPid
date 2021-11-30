f6547fa3ebc274a59c1669d97cb61bdaf3fb1110
Yipeng Sun <syp@umd.edu>
2021-11-30 15:56:57 +0100

    Add a Python virtualenv for plotting.

--- flake.nix
+++ flake.nix
@@ -18,15 +18,40 @@
           inherit system;
           overlays = [ self.overlay ];
         };
+        python = pkgs.python3;
+        pythonPackages = python.pkgs;
       in
       {
         devShell = pkgs.mkShell {
           pname = "MuonBDTPid";
 
-          buildInputs = with pkgs; [
+          buildInputs = (with pkgs; with pythonPackages; [
             root5-ubdt
             addUBDTBranchWrapped
-          ];
+
+            virtualenvwrapper
+            numpy
+          ]);
+
+          shellHook = ''
+            # Allow the use of wheels.
+            SOURCE_DATE_EPOCH=$(date +%s)
+            VENV=./.virtualenv
+
+            if test ! -d $VENV; then
+              virtualenv $VENV
+            fi
+            source $VENV/bin/activate
+
+            # Allow for the environment to pick up packages installed with virtualenv
+            export PYTHONPATH=$VENV/${python.sitePackages}/:$PYTHONPATH
+
+            # Fix libstdc++.so not found error
+            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+
+            # Update PATH
+            export PATH=$(pwd)/test:$(pwd)/workflows:$(pwd)/ganga:$(pwd)/scripts:$PATH
+          '';
         };
       });
 }
