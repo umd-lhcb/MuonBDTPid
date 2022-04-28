af44ebecaf401c267087ae90685ac0c1a82b5cdb
Emily Jiang <ejiang@umd.edu>
2022-04-28 16:59:30 -0400

    Cleaned up scripts

--- scripts/sha_checker.py
+++ scripts/sha_checker.py
@@ -1,39 +1,65 @@
+import os
+import yaml
+import hashlib
 
-with open('/home/ejiang/MuonBDTPid/hash_file_final.txt') as local,open("/home/ejiang/MuonBDTPid/mu_down.txt") as mu_down, open("/home/ejiang/MuonBDTPid/mu_up.txt") as mu_up, open("/home/ejiang/MuonBDTPid/kpi_down.txt") as kpi_down, open("/home/ejiang/MuonBDTPid/kpi_up.txt") as kpi_up, open("/home/ejiang/MuonBDTPid/p_down.txt") as p_down, open("/home/ejiang/MuonBDTPid/p_up.txt") as p_up:
-    mu_down_lines = mu_down.read().splitlines()
-    mu_up_lines = mu_up.read().splitlines()
-    kpi_down_lines = kpi_down.read().splitlines()
-    kpi_up_lines = kpi_up.read().splitlines()
-    p_down_lines = p_down.read().splitlines()
-    p_up_lines = p_up.read().splitlines()
-    read_local = local.read()
-    for line in mu_down_lines:
-        if line in read_local:
-            print(1)
+with open('/home/ejiang/MuonBDTPid/spec/pidcalib.yml', 'r') as stream:
+    files = yaml.safe_load(stream)
+
+for decay in files["data"]:
+    for folder in files["data"][decay]:
+        for mag in files["data"][decay][folder]:
+            localDir = files["local_ntuple_folders"][folder]+"/"+decay+"-"+mag
+            remoteDir = "ejiang@lxplus.cern.ch:"+files["data"][decay][folder][mag]+"/"
+            for filename in os.listdir(localDir):
+                fLocal = localDir+"/"+filename
+                with open(fLocal, "rb") as f:
+                    bytes = f.read()
+                    hash = hashlib.sha256(bytes).hexdigest()
+                    hash_file.write(hash+"\n")
+    
+with (
+        open('/home/ejiang/MuonBDTPid/hash_file_final.txt') as local,
+        open("/home/ejiang/MuonBDTPid/mu_down.txt") as muDown,
+        open("/home/ejiang/MuonBDTPid/mu_up.txt") as muUp,
+        open("/home/ejiang/MuonBDTPid/kpi_down.txt") as kpiDown,
+        open("/home/ejiang/MuonBDTPid/kpi_up.txt") as kpiUp,
+        open("/home/ejiang/MuonBDTPid/p_down.txt") as pDown,
+        open("/home/ejiang/MuonBDTPid/p_up.txt") as pUp
+):
+    muDownLines = muDown.read().splitlines()
+    muUpLines = muUp.read().splitlines()
+    kpiDownLines = kpiDown.read().splitlines()
+    kpiUpLines = kpiUp.read().splitlines()
+    pDownLines = pDown.read().splitlines()
+    pUpLines = pUp.read().splitlines()
+    readLocal = local.read()
+    for line in muDownLines:
+        if line in readLocal:
+            continue
         else:
             print(-1)
-    for line in mu_up_lines:
-        if line in read_local:
-            print(1)
+    for line in muUpLines:
+        if line in readLocal:
+            continue
         else:
             print(-1)
-    for line in kpi_down_lines:
-        if line in read_local:
-            print(1)
+    for line in kpiDownLines:
+        if line in readLocal:
+            continue
         else:
             print(-1)
-    for line in kpi_up_lines:
-        if line in read_local:
-            print(1)
+    for line in kpiUpLines:
+        if line in readLocal:
+            continue
         else:
             print(-1)
-    for line in p_down_lines:
-        if line in read_local:
-            print(1)
+    for line in pDownLines:
+        if line in readLocal:
+            continue
         else:
             print(-1)
-    for line in p_up_lines:
-        if line in read_local:
-            print(1)
+    for line in pUpLines:
+        if line in readLocal:
+            continue
         else:
             print(-1)
