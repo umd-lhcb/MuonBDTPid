3edbf328f653d140f9b25def6e7cf502e7a20078
Emily Jiang <ejiang@umd.edu>
2022-05-06 14:50:01 -0400

    Adding script for nix testing, cleaned up ntuple grabber

---
+++ scripts/nix_tester.py
@@ -0,0 +1,43 @@
+import argparse
+import yaml
+import os
+import uproot
+
+parser = argparse.ArgumentParser(description='Process yml filename.')
+parser.add_argument('--ymlName', type=str,
+                    help='path to YAML file containing directories of files to be downloaded')
+args = parser.parse_args()
+
+with open(args.ymlName, 'r') as stream:
+    files = yaml.safe_load(stream)
+    
+for decay in files["data"]:
+    for folder in files["data"][decay]:
+        i = 0
+        for mag in files["data"][decay][folder]:
+            inputDir = files["local_ntuple_folders"][folder]+"/"+decay+"-"+mag
+            outputDir = files["local_ntuple_folders"]["friends"]+"/"+decay+"-"+mag
+            #i = 0
+            os.system("mkdir -p "+outputDir)
+            for filename in os.listdir(inputDir):
+                fInput = inputDir+"/"+filename
+                fOutput = outputDir+"/"+filename
+                # Get names of trees--only need to do this once per decay, hence the counter
+                if i==0:
+                    rootFile = uproot.open(fInput)
+                    trees = []
+                    for tree in rootFile.keys():
+                        if "DecayTree" in tree:
+                            trees.append(tree.replace(';1',''))
+                            print(trees)
+                    i=1
+
+                #Call BDT
+                cmd = "AddUBDTBranchPidCalib -i "+fInput+" -o "+fOutput+" -p probe -b UBDT -t "
+                for tree in trees:
+                    cmd=cmd+tree+","
+                #cmd=cmd[:-1]
+                print(cmd)
+                ret_code = os.system(cmd)
+                if ret_code!=0:
+                    print("WARNING: "+cmd+" did not execute properly!")
--- scripts/ntuple_grabber.py
+++ scripts/ntuple_grabber.py
@@ -1,15 +1,29 @@
+#!/usr/bin/env python
+# Author: Emily Jiang
+# Last Change: Tues May 03, 2022 at 04:39 PM +0100
+#
+# Description: Parse a yaml file containing the location of files on lxplus,
+#              copying them to a glacier directory (also specified in yaml)
+
+import argparse
 import yaml
 import subprocess
-import os
 
-with open('/home/ejiang/MuonBDTPid/spec/pidcalib.yml', 'r') as stream:
+parser = argparse.ArgumentParser(description='Process yml filename.')
+parser.add_argument('--ymlName', type=str,
+                    help='path to YAML file containing directories of files to be downloaded')
+parser.add_argument('--lxplusUsr', type=str,
+                   help='your username for lxplus')
+args = parser.parse_args()
+
+with open(args.ymlName, 'r') as stream:
     files = yaml.safe_load(stream)
     
 for decay in files["data"]:
     for folder in files["data"][decay]:
         for mag in files["data"][decay][folder]:
             localDir = files["local_ntuple_folders"][folder]+"/"+decay+"-"+mag
-            remoteDir = "ejiang@lxplus.cern.ch:"+files["data"][decay][folder][mag]+"/"
+            remoteDir = args.lxplusUsr+"@lxplus.cern.ch:"+files["data"][decay][folder][mag]+"/"
             subprocess.run(["mkdir", "-p", localDir])
             subprocess.run(["rsync", "-a", remoteDir, localDir])    
            
