9e847b75a60ece673a717b19f2ee19383f29df27
Yipeng Sun <syp@umd.edu>
2022-06-08 02:40:19 -0400

    Grant +x permission; small cleanup for the ntuple grabber.

--- scripts/ntuple_grabber.py
+++ scripts/ntuple_grabber.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Author: Emily Jiang
-# Last Change: Tue Jun 07, 2022 at 09:29 PM -0400
+# Last Change: Wed Jun 08, 2022 at 02:40 AM -0400
 #
 # Description: Parse a yaml file containing the location of files on lxplus,
 #              copying them to a glacier directory (also specified in yaml)
@@ -14,10 +14,14 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument(
     "--ymlName",
-    type=str,
     help="path to YAML file containing directories of files to be downloaded",
 )
-parser.add_argument("--lxplusUser", type=str, help="your username for lxplus")
+parser.add_argument("--lxplusUser", help="your username for remote host")
+parser.add_argument(
+    "--host",
+    default="lxplus.cern.ch",
+    help="specify remote host name (default to lxplus)"
+)
 args = parser.parse_args()
 
 with open(args.ymlName, "r") as stream:
@@ -28,8 +32,7 @@ for decay in files["data"]:
         for mag in files["data"][decay][folder]:
             localDir = files["local_ntuple_folders"][folder] + "/" + decay + "-" + mag
             remoteDir = (
-                args.lxplusUsr
-                + "@lxplus.cern.ch:"
+                f"{args.lxplusUser}@{args.host}:"
                 + files["data"][decay][folder][mag]
                 + "/"
             )
--- scripts/sha_checker.py
+++ scripts/sha_checker.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 # Author: Emily Jiang
-# Last Change: Tues May 010, 2022 at 02:24 PM +0100
+# Last Change: Tue Jun 07, 2022 at 08:08 PM -0400
 #
-# Description: to use this script, first generate shasum txt files on lxplus with naming conventions outlined in
-# MuonBDTPid/spec/pidcalib.yml, e.g. Mu_nopt-2016-MagDown. Then download to your local copy of MuonBDTPid. I did this by
-# directory, splitting up the decays and the magnetization, since lxplus was complaining about memory usage. This script
-# checks the hash file generated for the original files with the copied files to make sure nothing went wrong.
+# Description: to use this script, first generate shasum txt files on lxplus
+#              with naming conventions outlined in MuonBDTPid/spec/pidcalib.yml,
+#              e.g. Mu_nopt-2016-MagDown. Then download to your local copy of
+#              MuonBDTPid.
+#
+#              I did this by directory, splitting up the decays and the
+#              magnetization, since lxplus was complaining about memory usage.
+#              This script checks the hash file generated for the original files
+#              with the copied files to make sure nothing went wrong.
+#
+# Instructions: on lxplus, generate shasum txt files with naming conventions
+#               outlined in MuonBDTPid/spec/pidcalib.yml, e.g.
+#               Mu_nopt-2016-MagDown. I did this one by one, since I was having
+#               trouble with memory usage on lxplus trying to do everything at
+#               once.
 
 import argparse
 import os
 import yaml
 import hashlib
 
-# Instructions: on lxplus, generate shasum txt files with naming conventions outlined in
-# MuonBDTPid/spec/pidcalib.yml, e.g. Mu_nopt-2016-MagDown. I did this one by one, since I
-# was having trouble with memory usage on lxplus trying to do everything at once.
 
 parser = argparse.ArgumentParser(description="Process yml filename.")
 parser.add_argument(
@@ -29,6 +37,7 @@ with open(args.ymlName, "r") as stream:
 
 local = open("/home/ejiang/MuonBDTPid/hash_file_final.txt")
 readLocal = local.read()
+
 
 for decay in files["data"]:
     for folder in files["data"][decay]:
