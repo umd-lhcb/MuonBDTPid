dbf4f0530649407761d605b5f2b1a1b641f770e8
Manuel Franco Sevilla <manuelf@umd.edu>
2024-08-26 11:05:35 -0400

    Updated uBDT script to remove ;# from tree names

--- scripts/apply_ubdt.py
+++ scripts/apply_ubdt.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-# Author: Emily Jiang
+# Author: Emily Jiang, Manuel Franco Sevilla
 # Last Change: Sat Aug 06, 2022 at 06:35 PM -0400
 #
 # Description: Apply the UBDT to root files in directories specified by a yml
 #              file (input), and write them to specified output directory.
 
+from subprocess import Popen, PIPE, STDOUT
 import argparse
 import yaml
 import os
@@ -14,42 +15,73 @@ from glob import glob
 from os.path import basename
 
 
-parser = argparse.ArgumentParser(description="Apply UBDT to PIDCalib ntuples.")
-parser.add_argument(
-    "--ymlName",
-    help="path to YAML file containing directories of files to be downloaded",
-)
-parser.add_argument("--dryRun", action="store_true", help="dry run")
+#################################
+# Command line arguments parser #
+#################################
 
-args = parser.parse_args()
+def parseInput():
+    parser = argparse.ArgumentParser(description="Apply UBDT to PIDCalib ntuples.")
+    parser.add_argument('-y', '--ymlName', help="path to YAML file containing directories of files to be downloaded")
+    parser.add_argument('-d', "--dryRun", action="store_true", help="dry run")
+
+    return parser.parse_args()
 
 
-with open(args.ymlName, "r") as stream:
-    config = yaml.safe_load(stream)
+## Add colors for terminal output
+def cTerm(msg, color):
+    num = 30
+    if color == 'red':     num = 91
+    if color == 'green':   num = 92
+    if color == 'yellow':  num = 93
+    if color == 'blue':    num = 94
+    if color == 'magenta': num = 95
+    if color == 'cyan':    num = 96
+    return f'\033[{num};1m{msg}\033[0m'
 
-for species, directive in config["data"].items():
-    for mag, remoteBaseDir in directive.items():
-        try:
-            inputDir = f"{config['local_ntuple_folders']['remote']}/{species}-{mag}/"
-            outputDir = f"{config['local_ntuple_folders']['friends']}/{species}-{mag}/"
-            os.system("mkdir -p " + outputDir)
-            print(f"{inputDir} -> {outputDir}")
+########
+# Main #
+########
 
-            for fInput in glob(inputDir + "*.root"):
-                fOutput = outputDir + basename(fInput)
-                rootFile = uproot.open(fInput)
-                trees = [t.replace(";1", "") for t in rootFile if "DecayTree" in t]
-                trees = ",".join(trees)
-                print(f"  trees: {trees}")
+if __name__ == '__main__':
+    args = parseInput()
+    
+    with open(args.ymlName, "r") as stream:
+        config = yaml.safe_load(stream)
+    
+    if not args.dryRun:
+        retCode = os.system('make')
+        if retCode != 0:
+            print(cTerm('Compilation failed', 'red'))
+            sys.exit(1)
+            
+    for species, directive in config["data"].items():
+        for mag, remoteBaseDir in directive.items():
+            try:
+                inputDir = f"{config['local_ntuple_folders']['remote']}/{species}-{mag}/"
+                outputDir = f"{config['local_ntuple_folders']['friends']}/{species}-{mag}/"
+                os.system("mkdir -p " + outputDir)
+                print(f"{inputDir} -> {outputDir}")
+    
+                for fInput in glob(inputDir + "*.root"):
+                    fOutput = outputDir + basename(fInput)
+                    rootFile = uproot.open(fInput)
 
-                # Call UBDT
-                cmd = f"  AddUBDTBranchPidCalib -i {fInput} -o {fOutput} -p probe -b UBDT -t {trees}"
-                print(cmd)
-
-                if not args.dryRun:
-                    retCode = os.system(cmd)
-                    if retCode != 0:
-                        print(f"  WARNING: {cmd} did not execute properly!")
-
-        except KeyboardInterrupt:
-            break
+                    ## Finding trees in the ntuple
+                    trees = []
+                    for tname in rootFile:
+                        if 'DecayTree' not in tname: continue
+                        trees.append(tname.split(';')[0]) # Remove the ";#" after the tree name
+                    trees = ",".join(trees)
+    
+                    # Call UBDT
+                    cmd = f'./bin/AddUBDTBranchRun2PidCalib -i {fInput} -o {fOutput} -p probe -b UBDT '
+                    cmd += f'-t {trees} -x weights/weights_run2_no_cut_ubdt.xml'
+    
+                    if not args.dryRun:
+                        print(cTerm(cmd,'magenta'))
+                        retCode = os.system(cmd)
+                        if retCode != 0:
+                            print(f"  WARNING: {cmd} did not execute properly!")
+    
+            except KeyboardInterrupt:
+                break
